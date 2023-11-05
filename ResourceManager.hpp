dcb0e57a036c37417b6f99cbffc9c5130c1aeac0
nalbor1337 <107367538+nalbor1337@users.noreply.github.com>
2023-11-05 15:03:45 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,44 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+        ResourceManager() : resource(new Resource()){}
+        
+        ResourceManager(const ResourceManager& r) : resource(new Resource(*(r.resource))){}
+
+        ResourceManager& operator=(const ResourceManager& r)
+        {
+            if (this != &r) {
+                delete resource;
+                resource = new Resource(*(r.resource));
+            }
+            return *this;
+        }
+
+        ResourceManager(ResourceManager&& r) : resource(r.resource){
+            r.resource = nullptr;
+        }
+
+        ResourceManager& operator=(ResourceManager&& r)
+        {
+            if (this != &r) {
+                delete resource;
+                resource = new Resource(*(r.resource));
+                r.resource = nullptr;
+            }
+            return *this;
+        }
+
+        ~ResourceManager()
+        {
+            delete resource;
+        }
+
+        double get()
+        {
+            return resource->get();
+        }
+
+    private:
+        Resource* resource;
 };
